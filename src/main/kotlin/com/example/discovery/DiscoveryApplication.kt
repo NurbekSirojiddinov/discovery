1a327666132412a491f67f30577a28d6433b6bc0
Nurbek Sirojiddinov <nurbek.sirojiddinov@livetalks.tv>
2022-12-01 23:33:44 +0500

    Authorization server, phase-1

--- src/main/kotlin/com/example/discovery/DiscoveryApplication.kt
+++ src/main/kotlin/com/example/discovery/DiscoveryApplication.kt
@@ -2,7 +2,9 @@ package com.example.discovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class DiscoveryApplication
 
